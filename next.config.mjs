29dd5b2df46ee406d6d3092ef6ec98ca0a200a34
Akash-Cogg <akash@cognitivegarage.ai>
2024-03-11 12:38:40 +0530

    update next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push(
+            {
+                test: /\.js|\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.js|\.jsx$/,
+                use: [
+                    {
+                        loader: `val-loader`,
+                    },
+                ],
+            },
+        );
+        return config;
+    },
+};
 export default nextConfig;
