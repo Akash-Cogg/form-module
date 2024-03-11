941a9a08425442d4ed5b59ef4b13c2e0e7ad7cf7
Akash-Cogg <akash@cognitivegarage.ai>
2024-03-11 10:38:37 +0530

    created a module to reuse form viewer app

---
+++ webpack.config.mjs
@@ -0,0 +1,28 @@
+import { join } from 'path';
+import webpack from 'webpack';
+
+export const entry = './src/app/page.js';
+export const output = {
+    path: join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+};
+export const resolve = {
+    extensions: ['.js', '.jsx']
+};
+// eslint-disable-next-line @next/next/no-assign-module-variable
+const module = {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader'
+        }
+    }
+    ]
+};
+export const devtool = 'cheap-module-eval-source-map';
+export const devServer = {
+    contentBase: join(__dirname, 'public')
+};
+export default module;
